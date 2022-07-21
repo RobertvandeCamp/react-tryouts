6f2b5dd31c9855437942863f3f7e71cfb3f7e9b2
RobertvandeCamp <robertvandecamp@gmail.com>
2022-07-21 20:25:19 +0200

    Nice integrations

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello my CodeSandbox</h1>
+      <h1>Hello again CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
